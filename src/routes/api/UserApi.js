cc5040d75866943931a1879c090227f8b151e5e9
LILLINH <102342993+LILLINH@users.noreply.github.com>
2023-11-01 16:06:24 +0700

    fix

---
+++ src/routes/api/UserApi.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+const UserController = require("../../controllers/UserController");
+
+// 192.168.1.53
+// http://42.116.118.69:3000/api/user/getUser
+router.get("/getUser", (req, res, next) => {
+  // try {
+  //     console.log('eeeeeeeeeeeeeee');
+  //     // const users = await UserController.getAllUser();
+  //     // return res.status(200).json({result: true, users: users});
+  // } catch (error) {
+  //     // return res.status(500).json({result: false, users: null});
+  // }
+  console.log("ddd");
+});
+
+module.exports = router;
