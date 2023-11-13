6448504682de2dfc8d211ef71606b8cdb09320f7
LILLINH <102342993+LILLINH@users.noreply.github.com>
2023-11-13 18:28:32 +0700

    done

--- src/controllers/web/hotelsController.js
+++ src/controllers/web/hotelsController.js
@@ -1,9 +1,73 @@
-const webService = require("../../services/web/hotelsService");
-const hotels = async (req, res) => {
-    const hotels = await webService.getAllHotels();
-    res.render("hotels.ejs", { hotels });
-  };
+const hotelsService = require("../../services/web/hotelsService");
 
+// get all hotels
+const getAllHotels = async (page) => {
+  try {
+    const hotels = await hotelsService.getAllHotels(page);
+    const numberOfPages = await hotelsService.getAllHotelsPage();
+    return { hotels, numberOfPages };
+  } catch (error) {
+    return error;
+  }
+};
+// delete hotel
+const deleteHotel = async (id) => {
+  try {
+    return await hotelsService.deleteHotel(id);
+  } catch (error) {
+    return false;
+  }
+};
+// post add hotel
+const postAddHotel = async (hotelName, address, images, star, price) => {
+  try {
+    return await hotelsService.addHotel(
+      hotelName,
+      address,
+      images,
+      star,
+      price
+    );
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+};
+
+// get hotel by id
+const getHotelById = async (id) => {
+  try {
+    return await hotelsService.getHotelById(id);
+  } catch (error) {
+    return false;
+  }
+};
+// post edit hotelm
+const postEditHotel = async (
+  id,
+        hotelName,
+        address,
+        images,
+        star,
+        price
+) => {
+  try {
+    return await hotelsService.postEditHotel(
+      id,
+        hotelName,
+        address,
+        images,
+        star,
+        price
+    );
+  } catch (error) {
+    return false;
+  }
+};
 module.exports = {
-    hotels,
+  getAllHotels,
+  deleteHotel,
+  postAddHotel,
+  getHotelById,
+  postEditHotel,
 };
---
+++ src/models/typeOfTourModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const typeOfTourSchema = new Schema({
+    _id: { type: ObjectId },
+    typeName: { type: String },
+    
+});
+
+module.exports = mongoose.model('typeOfTour', typeOfTourSchema);
