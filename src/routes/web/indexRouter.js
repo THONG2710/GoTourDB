e5f1739dd8683cb44cf0adb8f212fe8447910e05
Nguyen Van Thong <5506nguyenthong@gmail.com>
2023-11-20 18:37:01 +0700

    commit

--- src/routes/web/indexRouter.js
+++ src/routes/web/indexRouter.js
@@ -3,12 +3,16 @@ const signInRouter = require("./signInRouter");
 const usersRouter = require("./usersRouter");
 const hotelsRouter = require("./hotelsRouter");
 const toursRouter = require("./toursRouter");
+const trainsRouter = require('./trainsRouter');
+const airlinesRouter = require('./airlinesRouter');
 
 module.exports = {
     signInRouter,
     usersRouter,
     hotelsRouter,
-    toursRouter
+    toursRouter,
+    trainsRouter,
+    airlinesRouter
     };
 
 
